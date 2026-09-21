a3a4e211f8aa9ba8c8907d0f931ff4c6c7529087
Mohammed Emad <126204744+MohammedEmad333@users.noreply.github.com>
2026-09-21 05:02:12 +0300

    Add core/build.gradle.kts

---
+++ core/build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    id("com.android.library")
+}
+
+android {
+    compileSdk = AndroidConfig.compileSdk
+    namespace = AndroidConfig.coreNamespace
+
+    defaultConfig {
+        minSdk = AndroidConfig.minSdk
+    }
+
+    sourceSets {
+        named("main") {
+            manifest.srcFile("AndroidManifest.xml")
+            res.setSrcDirs(listOf("res"))
+        }
+    }
+}
